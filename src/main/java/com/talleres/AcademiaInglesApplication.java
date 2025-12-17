e95b5bbc2acf330ac00407a5320ab97017c4bc94
ramijos <amijosroy9@gmail.com>
2025-12-17 09:39:17 -0500

    Roy

---
+++ src/main/java/com/talleres/AcademiaInglesApplication.java
@@ -0,0 +1,13 @@
+package com.talleres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiaInglesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiaInglesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/talleres/AcademiaInglesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talleres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiaInglesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
