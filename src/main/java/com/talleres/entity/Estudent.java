5c5c489614d03c1b774190f55329f104c3569d2a
lportuano <luchoportuano460@gmail.com>
2025-12-18 23:47:16 -0500

    Formulario Roy

--- src/main/java/com/talleres/entity/Estudent.java
+++ src/main/java/com/talleres/entity/Estudent.java
@@ -1,9 +1,6 @@
 package com.talleres.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -36,6 +33,7 @@ public class Estudent {
     @NotNull(message = "La fecha de nacimiento es obligatoria")
     @Past(message = "La fecha de nacimiento debe ser en el pasado")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Temporal(TemporalType.DATE)
     private Date fechaNacimiento;
 
     @NotBlank(message = "El email es obligatorio")
@@ -43,9 +41,14 @@ public class Estudent {
     @Size(max = 100, message = "El email no puede exceder los 100 caracteres")
     private String email;
 
+
     @NotBlank(message = "El teléfono es obligatorio")
-    @Pattern(regexp = "^[0-9+\\s()-]{7,20}$", message = "Teléfono inválido. Ejemplo: +34 123 456 789")
+    @Pattern(
+            regexp = "^[0-9]{10}$",
+            message = "El teléfono debe contener exactamente 10 dígitos numéricos"
+    )
     private String telefono;
+
 
     @NotBlank(message = "La dirección es obligatoria")
     @Size(min = 5, max = 200, message = "La dirección debe tener entre 5 y 200 caracteres")
@@ -53,6 +56,7 @@ public class Estudent {
 
     @NotNull(message = "La fecha de inscripción es obligatoria")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Temporal(TemporalType.DATE)
     private Date fechaInscripcion;
 
     @NotBlank(message = "El nivel de inglés es obligatorio")
